6f4637fb3cd48ec3efdb084e8bf2de4085a8d46d
Jean-Baptiste Mazon <jb@xwing>
2014-08-23 12:32:50 +0200

    Oops

--- lib/Acme/Template/TuringComplete.pm
+++ lib/Acme/Template/TuringComplete.pm
@@ -18,7 +18,7 @@ sub interpret {
       '-' => '$ram[$p]--;',
       '>' => '$p++;',
       '<' => '$p--;',
-      '[' => '{my$r=$ram[$p];while($ram[$p]=ref$r?shift@$r:looks_like_number$r?$ram[$p]:ord substr$r//"\0",0,1,""){',
+      '[' => '{my$r=$ram[$p];while($ram[$p]=ref$r?shift@$r:looks_like_number$r?$ram[$p]:defined$r?ord substr$r,0,1,"":0){',
       ']' => '}}',
       '.' => '$output.=defined$ram[$p]&&looks_like_number$ram[$p]?chr$ram[$p]:$ram[$p];',
       ',' => '$ram[$p]=shift@args;',
