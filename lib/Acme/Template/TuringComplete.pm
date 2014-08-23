c447dea9bf860f9d78c3bfeffae31e3574f2d436
Jean-Baptiste Mazon <jb@xwing>
2014-08-23 10:55:36 +0200

    Make a module out of it

---
+++ lib/Acme/Template/TuringComplete.pm
@@ -0,0 +1,39 @@
+#! /usr/bin/env perl
+
+package Acme::Template::TuringComplete;
+use 5.010;
+use strict;
+use warnings;
+
+use Scalar::Util qw( looks_like_number );
+
+use Exporter;
+our @EXPORT = qw(interpret);
+
+sub interpret {
+  my $class = shift;
+  my %translation =
+    (
+      '+' => '$ram[$p]++;',
+      '-' => '$ram[$p]--;',
+      '>' => '$p++;',
+      '<' => '$p--;',
+      '[' => '{my$r=$ram[$p];while($ram[$p]=ref$r?shift@$r:looks_like_number$r?$ram[$p]:ord substr$r//"\0",0,1,""){',
+      ']' => '}}',
+      '.' => '$output.=defined$ram[$p]&&looks_like_number$ram[$p]?chr$ram[$p]:$ram[$p];',
+      ',' => '$ram[$p]=shift@args;',
+    );
+  my ($template,@args) = @_;
+  my $program = '';
+  for my $c ( $template =~ /./gs ) {
+    my $op = $translation{$c};
+    $program .= $op // "\$output .= '$c';";
+  }
+
+  my @ram;
+  my $p = 0;
+  my $output = '';
+
+  eval $program;
+  $output;
+}
